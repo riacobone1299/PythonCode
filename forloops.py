ee25fc700103b232ea6d5910bd7c8dd185c6d167
Rocco Iacobone <roccoiacobone@Roccos-MacBook-Pro.local>
2024-12-20 18:11:53 -0500

    objects

---
+++ forloops.py
@@ -0,0 +1,8 @@
+cars = ["Ford", "Volvo", "BMW"]
+print(cars[0])
+print(cars[1])
+print(cars[2])
+x = len(cars)
+print(x)
+for variable in cars: 
+    print(variable)
---
+++ objects.py
@@ -0,0 +1,9 @@
+class Person: 
+    def __init__(self, name, age, height): 
+        self.name = name 
+        self.age = age
+        self.height = height
+p1 = Person("John", 36, 5.7)
+print(p1.name)
+print(p1.age) 
+print(p1.height)
