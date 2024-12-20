35f1025edf719587fbab133c016e83d6aa131cb5
Rocco Iacobone <roccoiacobone@Roccos-MacBook-Pro.local>
2024-12-20 17:48:04 -0500

    if statements and packages

---
+++ If.py
@@ -0,0 +1,11 @@
+i = 100
+if i < 100:
+    print("number<100") 
+else:
+    print("number 100 or greater")    
+if i < 100: 
+    print("number<100")
+elif i == 100:
+    print("number i = 100")
+else:
+    print("i > 100")    
--- Python.py
+++ Python.py
@@ -1,1 +1,19 @@
-print('hello world)')
+#print('hello world)')
+mynumbr = 2
+x = 6
+print(mynumbr)
+answer = mynumbr * x
+print(answer)
+#note:functionbelow
+def addtwonumbers(number1,number2):
+    answer = number1 + number2
+    return answer
+z = addtwonumbers(8,9)
+print(z)
+#multiplpyfunction
+def multtwonumbers(number1,number2):
+    answer = number1 * number2
+    return answer
+z = multtwonumbers(15,17)
+print(z)
+#note: python overrides variables (i.e. "z") therefore, change it up (i.e. u, f, y)
