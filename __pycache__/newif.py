ff3710fa54376c00805806389f5f62ba593b8a3d
Rocco Iacobone <roccoiacobone@Roccos-MacBook-Pro.local>
2024-12-28 16:03:33 -0500

    refresh

---
+++ __pycache__/newif.py
@@ -0,0 +1,12 @@
+y = 75
+if y < 75:
+    print("number<75")
+else:
+    print("number 75 or greater")
+if y < 75: 
+    print("number < 75")
+elif y == 75:
+    print("number y = 75")
+else:
+    print("y > 75")     
+#indenting too far could cause an error in the code. 
---
+++ __pycache__/refresh.py
@@ -0,0 +1,5 @@
+def dividetwonumbers (number1, number2):
+    answer = number1 / number2 
+    return answer
+f = dividetwonumbers(365,12)
+print(f)
